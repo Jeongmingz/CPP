09aa5542b7d8f8ed9a3b3d609c9827909a9255f3
Jeongmin <joungmin0861@naver.com>
2023-03-09 11:55:26 +0900

    소멸자 및 this 명령어를 이용한 CPP 의 stack영역

--- VS/week02/week02_01_chap07_01.cpp
+++ VS/week02/week02_01_chap07_01.cpp
@@ -8,21 +8,26 @@ private:
 public:
     Circle(); // constructer 기본 생성자
     Circle(double value);
+    ~Circle(); // 소멸자
     double getRadius() const;
     double getArea() const;
     double getPerimeter() const;
     void setRadius(double value);
 };
-
-Circle::Circle() 
+Circle::~Circle()
 {
-    cout << "객체 생성, 기본생성자 \n";
+    cout << this << "===객체 소멸===\n";
+}
+Circle::Circle()
+{
+    cout << this << "객체 생성, 기본생성자 \n";
 }
 Circle::Circle(double value)
 {
-    cout << "객체 생성, 인스턴스가 있는생성자 \n";
+    cout << this << "객체 생성, 인스턴스가 있는생성자 \n";
     radius = value;
 }
+
 double Circle::getRadius() const
 {
     return radius;
@@ -42,11 +47,23 @@ void Circle::setRadius(double value)
     radius = value;
 }
 
+// 멤버 함수가 아닌 일반 함수
+void test()
+{
+    cout << "Circle 4" << endl;
+    Circle circle4;
+    circle4.setRadius(100.0);
+    cout << "반지름: " << circle4.getRadius() << endl;
+}
+
 int main()
 {
     // 첫 번째 circle 객체를 만들고 멤버 함수 호출
     cout << "Circle 1" << endl;
     Circle circle1;
+    
+    test();
+
     circle1.setRadius(10.0);
     cout << "반지름: " << circle1.getRadius() << endl;
     cout << "넓이: " << circle1.getArea() << endl;
@@ -65,6 +82,6 @@ int main()
     circle2.setRadius(20.0);
     cout << "반지름: " << circle2.getRadius() << endl;
     cout << "넓이: " << circle2.getArea() << endl;
-    cout << "둘레: " << circle2.getPerimeter();
+    cout << "둘레: " << circle2.getPerimeter() << endl << endl;
     return 0;
 }
