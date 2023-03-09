411fa38ce63adabada9f38f221dcb2db1765f614
Jeongmin <joungmin0861@naver.com>
2023-03-09 10:58:44 +0900

    visual studo chap7 code

---
+++ VS/week02/week02_01_chap07_01.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+private:
+    double radius; // 필드, 맴버변수, 데이터맴버, 속성
+public:
+    Circle(); // constructer 기본 생성자
+    Circle(double value);
+    double getRadius() const;
+    double getArea() const;
+    double getPerimeter() const;
+    void setRadius(double value);
+};
+
+Circle::Circle() 
+{
+    cout << "객체 생성, 기본생성자 \n";
+}
+Circle::Circle(double value)
+{
+    cout << "객체 생성, 인스턴스가 있는생성자 \n";
+    radius = value;
+}
+double Circle::getRadius() const
+{
+    return radius;
+}
+double Circle::getArea() const
+{
+    const double PI = 3.14;
+    return (PI * radius * radius);
+}
+double Circle::getPerimeter() const
+{
+    const double PI = 3.14;
+    return (2 * PI * radius);
+}
+void Circle::setRadius(double value)
+{
+    radius = value;
+}
+
+int main()
+{
+    // 첫 번째 circle 객체를 만들고 멤버 함수 호출
+    cout << "Circle 1" << endl;
+    Circle circle1;
+    circle1.setRadius(10.0);
+    cout << "반지름: " << circle1.getRadius() << endl;
+    cout << "넓이: " << circle1.getArea() << endl;
+    cout << "둘레: " << circle1.getPerimeter() << endl << endl;
+    // 두 번째 circle 객체를 만들고 멤버 함수 호출  
+
+    cout << "Circle 2" << endl;
+    Circle circle2;
+    circle2.setRadius(20.0);
+    cout << "반지름: " << circle2.getRadius() << endl;
+    cout << "넓이: " << circle2.getArea() << endl;
+    cout << "둘레: " << circle2.getPerimeter() << endl << endl;
+
+    cout << "Circle 3" << endl;
+    Circle circle3;
+    circle2.setRadius(20.0);
+    cout << "반지름: " << circle2.getRadius() << endl;
+    cout << "넓이: " << circle2.getArea() << endl;
+    cout << "둘레: " << circle2.getPerimeter();
+    return 0;
+}
