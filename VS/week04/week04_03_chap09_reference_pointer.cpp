eada6cbf0ae159f4b47733ba87dbe75953c0c436
Jeongmin <joungmin0861@naver.com>
2023-03-23 12:36:04 +0900

    23-03-23 CPP class files

--- VS/week04/week04_03_chap09_reference_pointer.cpp
+++ VS/week04/week04_03_chap09_reference_pointer.cpp
@@ -20,6 +20,9 @@ void squares(int& ra) { // pass by reference
 // [2] 래퍼런스는 반드시 선언과 동시에 어떤 변수의 별명이 될 것인지 결정해야 한다.
 // [3] 래퍼런스는 해당 타입의 리터럴 값으로 초기화 될 수 없다.
 // [4] 3번 사항의 예외로 const 키워드를 레퍼런스 앞에 붙이면 초기화 할 수 있다.
+// [5] 레퍼런스는 원본 변수의 타입과 일치해야 한다.
+// [6] 한 변수에 대해 레퍼런스 변수는 여러개 일 수 있다.
+// [7] 원본 변수가 const라면 레퍼런스 변수도 반드시 const해야 한다.
 
 
 // Option.2 : Pointer
@@ -32,6 +35,9 @@ void squares(int& ra) { // pass by reference
 // &: 주는 놈 *: 받는 놈
 
 int main() {
+
+	int num = 100;
+	int& rNum1 = num;
 
 	int* p;
 	//int* p = nullptr
