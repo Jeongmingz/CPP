093e03631736dbc63888b2a2e4b7cb9f477b9db4
Jeongmin <joungmin0861@naver.com>
2023-04-06 13:58:11 +0900

    23.04.06 CPP end

---
+++ VS/week06/week06_03_chap10_cstring.cpp
@@ -0,0 +1,17 @@
+//// week06_02_chap09_memory.cpp
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char a[] = "daelim";
+//	// 배열의 이름과 배열의 맨앞의 주소와 같다.
+//	cout << a << "\n";
+//	cout << &a[0] << "\n";
+//	
+//	char* pa = a; // char* pa = &a[0]
+//	cout << *pa << "\n";
+//	cout << (int)*(pa+6) << "\n";
+//	cout << (int)a[6] << "\n";
+//
+//	return 0;
+//}
